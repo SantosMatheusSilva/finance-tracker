7e3aec0fb4b71e377425376568acf59006f45aff
SantosMatheusSilva <mathsantos52@gmail.com>
2025-05-06 15:14:14 +0100

    fix userSchema validaditon

--- src/app/lib/db/schemas/userSchemas.ts
+++ src/app/lib/db/schemas/userSchemas.ts
@@ -7,17 +7,10 @@ export const createUserSchema = z.object({
     username: z.string().min(3, {message: 'Username must be at least 3 characters long' }).max(20, { message: 'Username must be at most 20 characters long' }),
     email: z.string().email('Invalid email address'),
     password: z.string().min(6, 'Password must be at least 6 characters long'),
-    confirmPassword: z.string().min(6, 'Please confirm your password').superRefine((val, ctx) => {
-        if (ctx.data.password !== val) {
-            ctx.addIssue({
-                code: z.ZodIssueCode.custom,
-                message: 'Passwords do not match',
-                path: ['confirmPassword'],
-            });
-        }
-        
-    },),
-
+    confirmPassword: z.string().min(6, 'Please confirm your password'),
+}).refine((data) => data.password === data.confirmPassword, {
+    message: 'Passwords do not match',
+    path: ['confirmPassword'],
 });
 
 // CreateUser type infered from zod createUserSchema
